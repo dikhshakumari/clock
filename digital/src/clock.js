2b5ac8b4603923411d9dea81cd301ee39c6725cb
dikhshakumari <bajajb852@gmail.com>
2024-11-04 10:37:55 +0530

    Add .gitattributes for line endings

---
+++ digital/src/clock.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
+function DigitalClock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const formattedTime = time.toLocaleTimeString();
+
+  return (
+    <div className="clock-container">
+      {formattedTime}
+    </div>
+  );
+}
+
+export default DigitalClock;
